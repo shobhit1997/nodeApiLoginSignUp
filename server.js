c369857911c136ab940a70b846727570c5c32136
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-06-22 14:16:01 +0530

    added header allow

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
- var express =	require('express');
+var express =	require('express');
 var app		=	express();
 var bodyParser = require('body-parser');
 var mongoose	=	require('mongoose');
@@ -9,6 +9,25 @@ mongoose.connect('mongodb://root:root1234@ds261460.mlab.com:61460/document_users
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.static('./public'));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 var port= process.env.PORT||8080;
 
