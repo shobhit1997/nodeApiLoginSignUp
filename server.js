0c0e4bc1ca5f0cc39af8043b24c6be54f098cdcd
Shobhit Agarwal <shobhit.shobhitagarwal.agarwal@gmail.com>
2018-06-22 14:40:04 +0530

    added header allow

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('x-auth', '*');
 
     // Request methods you wish to allow
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept,x-auth');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
